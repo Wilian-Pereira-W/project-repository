e2de9b07873cff9342f5939681f4bf00f7790702
Wilian Pereira <wilian_pereira92@outlook.com>
2022-08-26 11:23:09 -0300

    feat: implementado a criação state newRepo e as funções handleInputChange e handleSubmit

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,19 +1,31 @@
 import styles from './styles.module.scss';
 import { FaGithub, FaPlus } from 'react-icons/fa';
+import { SyntheticEvent, useState } from 'react';
 function Main() {
+  const [newRepo, setNewRepo] = useState<string>('');
+
+  const handleInputChange = (value: string) => {
+    setNewRepo(value);
+  };
+
+  const handleSubmit = (e: SyntheticEvent) => {
+    e.preventDefault();
+    console.log(newRepo);
+  };
+
   return (
     <div className={styles.container}>
       <h1>
         <FaGithub size={25} />
         Meus Repositórios
       </h1>
-      <form
-        onSubmit={() => {
-          console.log('oi');
-        }}
-        className={styles.contentForm}
-      >
-        <input type="text" id="Adicionar Repositórios" />
+      <form onSubmit={(e) => handleSubmit(e)} className={styles.contentForm}>
+        <input
+          type="text"
+          id="Adicionar Repositórios"
+          value={newRepo}
+          onChange={({ target }) => handleInputChange(target.value)}
+        />
         <button type="submit" className={styles.contentFormButton}>
           <FaPlus color="#FFF" size={14} />
         </button>
