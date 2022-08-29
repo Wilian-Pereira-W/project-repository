86857a8c5a4b10edb55c3e1ac6e377962d8a343b
Wilian Pereira <wilian_pereira92@outlook.com>
2022-08-29 16:10:00 -0300

    feat: implementado o header

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { FaArrowLeft } from 'react-icons/fa';
+import { Link, useParams } from 'react-router-dom';
+import { IUserRepository } from '../../interface/userRepository';
 import api from '../../service/api';
 import styles from './styles.module.scss';
 function Repository() {
   const params = useParams<{ repository: string }>();
-  const [repository, setRepository] = useState({});
+  const [repository, setRepository] = useState<IUserRepository>({});
   const [issues, setIssues] = useState([]);
   const [loading, setLoading] = useState<boolean>(true);
 
@@ -29,9 +31,27 @@ function Repository() {
 
     load();
   }, [params]);
+
+  if (loading) {
+    return (
+      <div className={styles.loading}>
+        <h1>Carregado...</h1>
+      </div>
+    );
+  }
+
+  console.log(issues);
+
   return (
     <div className={styles.container}>
-      <h1>Repository</h1>
+      <Link to="/">
+        <FaArrowLeft color="#000" size={35} />
+      </Link>
+      <header className={styles.owner}>
+        <img src={repository.owner?.avatar_url} alt={repository.owner?.login} />
+        <h1>{repository.name}</h1>
+        <p>{repository.description}</p>
+      </header>
     </div>
   );
 }
